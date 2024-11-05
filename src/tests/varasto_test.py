1bbdf5ee4974b3359571b03035d5626808df8f6b
harilmsl <harilmsl@student.jyu.fi>
2024-11-05 19:47:34 +0200

    vaihe 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -3,6 +3,49 @@ from varasto import Varasto
 
 
 class TestVarasto(unittest.TestCase):
+    ## Uudet testit
+
+    def test_negatiivinen_alku_saldo(self):
+        testiVarasto = Varasto(5,-10)
+        self.assertEqual(testiVarasto.saldo, 0)
+
+    def test_liian_iso_alku_saldo(self):
+        testiVarasto = Varasto(20, 25)
+        self.assertEqual(testiVarasto.saldo, 20)
+
+    def test_negatiivinen_tilavuus(self):
+        testiVarasto = Varasto(-5,0)
+        self.assertEqual(testiVarasto.tilavuus, 0)
+
+    def test_negatiivinen_lisaa(self):
+        testiVarasto = Varasto(10,5)
+        testiVarasto.lisaa_varastoon(-5)
+        self.assertEqual(testiVarasto.saldo, 5)
+
+    def test_liian_suuri_lisaa(self):
+        testiVarasto = Varasto(10,5)
+        testiVarasto.lisaa_varastoon(10)
+        self.assertEqual(testiVarasto.saldo, 10)
+
+    def test_negatiivinen_nosto(self):
+        testiVarasto = Varasto(10,5)
+        testiVarasto.ota_varastosta(-5)
+        self.assertEqual(testiVarasto.saldo, 5)
+
+    def test_liian_suuri_nosto(self):
+            testiVarasto = Varasto(10,5)
+            testiVarasto.ota_varastosta(10)
+            self.assertEqual(testiVarasto.saldo, 0)
+
+    def test_to_string(self):
+            testiVarasto = Varasto(15,10)
+            self.assertEqual(str(testiVarasto), "saldo = 10, vielä tilaa 5")
+
+            #"saldo = 10.0, vielä tilaa 5.0"
+
+
+    ## Alkuperäiset testit
+
     def setUp(self):
         self.varasto = Varasto(10)
 
