58caabe17d2af75de653d9007b29ab0e7471ed8f
harilmsl <harilmsl@student.jyu.fi>
2024-11-05 19:55:59 +0200

    Rikki olevan koodin testaus

--- src/varasto.py
+++ src/varasto.py
@@ -18,7 +18,7 @@ class Varasto:
 
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
-        return self.tilavuus - self.saldo
+        return self.tilavuus + self.saldo
 
     def lisaa_varastoon(self, maara):
         if maara < 0:
